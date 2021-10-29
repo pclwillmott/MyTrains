45a3a968993db6a613ae18e4ddf677cc71e76e15
pclwillmott <pwillmott@northrock.bm>
2021-10-29 11:06:34 -0300

    Initial Commit

---
+++ MyTrains/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyTrains
+//
+//  Created by Paul Willmott on 29/10/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
