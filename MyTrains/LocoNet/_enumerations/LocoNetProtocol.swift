32556bf3b01486778cea80fc9c4c60b622a0a97f
pclwillmott <pwillmott@northrock.bm>
2023-06-22 15:02:35 -0300

    InterfaceLocoNet

---
+++ MyTrains/LocoNet/_enumerations/LocoNetProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  LocoNetProtocol.swift
+//  MyTrains
+//
+//  Created by Paul Willmott on 22/06/2023.
+//
+
+import Foundation
+
+public enum LocoNetProtocol {
+  
+  case protocol0
+  case protocol1
+  case protocol2
+  
+}
