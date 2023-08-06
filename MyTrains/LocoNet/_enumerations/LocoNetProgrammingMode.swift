42ef289d2fb4d3a44d1fe0261316789ddd7755c1
pclwillmott <pwillmott@northrock.bm>
2023-08-06 15:44:24 -0300

    Work on OpsMode

--- MyTrains/LocoNet/_enumerations/LocoNetProgrammingMode.swift
+++ MyTrains/LocoNet/_enumerations/LocoNetProgrammingMode.swift
@@ -9,15 +9,12 @@ import Foundation
 
 public enum LocoNetProgrammingMode : UInt8 {
   
-  case directModeByte                 = 0b00101011
-  case directModeBit                  = 0b00001011
-  case operationsModeByteNoFeedback   = 0b00100111
-  case operationsModeBitNoFeedback    = 0b00000111
-  case operationsModeByteWithFeedback = 0b00101111
-  case operationsModeBitWithFeedback  = 0b00001111
-  case pagedModeByte                  = 0b00100011
-  case pagedModeBit                   = 0b00000011
-  case physicalRegisterByte           = 0b00010011
+  case pagedMode          = 0b00100011
+  case directModeByte     = 0b00101011
+  case directModeBit      = 0b00001011
+  case operationsModeByte = 0b00101111
+  case operationsModeBit  = 0b00001111
+  case physicalRegister   = 0b00110011
 
   public var writeCommand : UInt8 {
     return self.rawValue | 0b01000000
